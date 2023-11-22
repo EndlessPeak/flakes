dd578ea10d2d526d810b4a7ba365c12309b8b456
EndlessPeak <endlesspeak@163.com>
2023-11-22 12:46:49 +0800

    fix sddm and code part

--- modules/desktop/sddm-theme.nix
+++ modules/desktop/sddm-theme.nix
@@ -1,13 +1,12 @@
 # sddm-theme-nix
 { pkgs }:
 
-# let
-#     imgLink = "<https>";
-#     image = pkgs.fetchurl {
-#         url = imgLink;
-#         sha256 = "";
-#     };
-# in
+let
+    image = pkgs.fetchurl {
+        url = "https://z1.ax1x.com/2023/11/18/piNMTFs.jpg";
+        sha256 = "02pgyhmm42bdx31yn64vbmyx7cfnn89zswj40rwr32fdgz83bxy7";
+    };
+in
 pkgs.stdenv.mkDerivation {
     name = "sddm-theme";
     # src = pkgs.fetchFromGitHub {
@@ -28,10 +27,12 @@ pkgs.stdenv.mkDerivation {
     };
     installPhase = ''
         mkdir -p $out
-        cp -R ./* $out/
+        cp -R ./* $out/ 
+        cd $out/Backgrounds
+        cp -r ${image} $out/Backgrounds/magenta.jpg
+        cd $out
+        sed -i 's/Background="Backgrounds\/Mountain.jpg"/Background="Backgrounds\/magenta.jpg"/g' theme.conf
+
     '';
         # the following are commented
-        # cd $out/
-        # rm Background.jpg
-        # cp -r ${image} $out/Background.jpg
 }
--- modules/develop/code.nix
+++ modules/develop/code.nix
@@ -1,20 +1,29 @@
 { config, lib, pkgs, pkgs-unstable, ... }:
 
 {
-  environment.systemPackages = 
-    (with pkgs;[
-      # Language Analysis
-      # sbcl # required by emacs lsp vfork
-      universal-ctags
+    environment.systemPackages = 
+        (with pkgs;[
+            # Language Analysis
+            # sbcl # required by emacs lsp vfork
+            universal-ctags
 
-      # Rust
-      rustup
+            # Rust
+            rustup
+        ]) ++
+        (with pkgs-unstable;[
+            # stm32cubemx
 
-      # IDE
-      jetbrains.clion
+            # IDE
+            (jetbrains.clion.overrideAttrs (old:{
+                postFixup = ''
+                    ${old.postFixup}
 
-    ]) ++
-    (with pkgs-unstable;[
-      # stm32cubemx
-    ]);
+                    # add ja-netfilter
+                    cd $out/clion/bin
+                    echo '-javaagent:/home/leesin/Downloads/jetbra/ja-netfilter.jar=jetbrains' >> clion64.vmoptions
+                    echo '--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED' >> clion64.vmoptions
+                    echo '--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED' >> clion64.vmoptions
+                '';
+            }))
+        ]);
 }
